31b5fd9cc5484244aa55d56947ef9f502aea1cbd
Tony Rizko <tony@rizkocircle.com>
2024-03-04 23:00:44 -0800

    Update main chat endpoint to use witness model

--- src/main.py
+++ src/main.py
@@ -5,32 +5,29 @@ import os
 import uvicorn
 
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from pydantic import BaseModel
+
+from models.witness import Witness
 
 
 load_dotenv()
 logger = logging.getLogger("uvicorn")
 
 
-# initialize the chain of langchain components
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are world class technical documentation writer."),
-    ("user", "{input}")
-])
 llm = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
-output_parser = StrOutputParser()
-chain = prompt | llm | output_parser
-
-
+witness = Witness(llm)
 app = FastAPI()
 
+class InputText(BaseModel):
+    input_text: str
 
-@app.get("/chat/")
-async def generate_text():
-    return chain.invoke({"input": "I need to write a technical documentation for a new software."})
+@app.post("/chat/")
+async def generate_text(input_text: InputText):
+    return witness.invoke({"input": input_text})
 
 
 if __name__ == "__main__":
